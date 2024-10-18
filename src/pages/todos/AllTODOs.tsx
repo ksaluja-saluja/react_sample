48793562af09ec5129bf8b6aa6a9af297417956d
ksaluja-saluja <saluja_kamal2000@yahoo.com>
2024-10-19 09:43:52 +1100

    fetch using custom hook

--- src/pages/todos/AllTODOs.tsx
+++ src/pages/todos/AllTODOs.tsx
@@ -1,37 +1,19 @@
-import { useEffect, useState } from "react";
 import SearchTODOs from "./Search";
 import ResultTODOs from "./Result";
 import Navbar from "../../components/Navbar";
-
-const delay = () => {
-  return new Promise((resolve) => setTimeout(resolve, 3000));
-};
+import { useGetTodos } from "../hooks/useGetTodos";
 
 const AllTodos = () => {
-  const [total, setTotal] = useState<number | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (!total) {
-      setLoading(true);
-      fetch("https://jsonplaceholder.typicode.com/todos/")
-        .then((response) => response.json())
-        .then((data) => {
-          delay().then(() => {
-            setTotal(data?.length);
-            setLoading(false);
-          });
-        });
-    }
-  }, [total]);
+  const { total, todos, loading } = useGetTodos({
+    url: "https://jsonplaceholder.typicode.com/todos",
+  });
 
   return (
     <>
       <Navbar></Navbar>
       <div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
+        {loading && <p>Loading details ...</p>}
+        {total && todos && (
           <>
             <p>Total TODOs are {total}</p>
             <div>
